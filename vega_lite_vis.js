0561a9124a002b66d95b2ac6770241df03c25af3
jess11111 <50970765+jess11111@users.noreply.github.com>
2021-10-09 20:17:56 +1100

    Update vega_lite_vis.js

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -2,3 +2,8 @@ var vg_1 = "choropleth2.vg.json";
 vegaEmbed("#map", vg_1).then(function(result) {
  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+var vg_2 = "bubble2.vega";
+vegaEmbed("#bubble", vg_2).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
